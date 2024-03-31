d5fcc7aa232c87d2f82d69d44d498ddd11fefed3
ashish.saha <rubelsaha1@gmail.com>
2024-03-31 15:39:14 +0600

    save only

--- LWS-8_Node Project/lib/data.js
+++ LWS-8_Node Project/lib/data.js
@@ -52,27 +52,6 @@ lib.create = function(dir, file, data, callback){
     })
 }
 
-// lib.create = (dir, file, data, callback)=>{
-//     fs.open(lib.baseDir+dir+'/'+file+'.json', 'wx', (err1, fileDescriptor)=>{
-//         if(!err1 && fileDescriptor){
-//             const stringData = JSON.stringify(data);
-//             fs.writeFile(fileDescriptor, stringData, (err2)=>{
-//                 if(!err2){
-//                     fs.close(fileDescriptor, (err3)=>{
-//                         if(!err3){
-//                             callback(false)
-//                         }else{
-//                             callback(err3)
-//                         }
-//                     })
-//                 }else{
-//                     callback(err2)
-//                 }
-//             })
-//         }else{
-//             callback(err1) 
-//         }
-//     })
-// }
+
 
 module.exports = lib;
