da8429b5a82a7f48ebb8915b66f1b322d2d4f881
Ashish Saha <rubelsaha1@gmail.com>
2024-04-22 23:17:23 +0600

    Lesson-6 running  from home

---
+++ LWS-8_Node Project/helper/notification.js
@@ -0,0 +1,28 @@
+/**
+ * Title: Notifications library
+ * Description: important function to notify users
+ * Author: Ashish Saha
+ * Date: 2024-04-22
+ */
+
+//Dependancies
+
+const https = require('https');
+
+
+//Module scaffolding
+
+const notifications =  {};
+
+
+//send sms to using twilio api
+
+notifications.sendTwilioSms = (phone, msg, callback)=>{
+
+}
+
+
+
+//Export module
+
+module.exports = notifications;
